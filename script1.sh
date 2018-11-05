9a8c1f21845d82ce09bfb1676a0dd8679b618473
Giannis Mavroudopoulos <mavroudo@csd.auth.gr>
2018-11-05 11:43:35 +0100

    working....

--- script1.sh
+++ script1.sh
@@ -1,7 +1,7 @@
  #!/bin/bash
 
 filename="tempResults.txt"
-tempFile="temp.txt"
+tempFile="tempWGET.txt"
 finalFile="results.txt"
 #checking for a file to open
 if [ $# -ne 1 ]
@@ -14,16 +14,25 @@ test -f $finalFile|| touch $finalFile
 
 for i in `cat "$1" | grep "^[^#]"`; do
 
-  wget -q $i -O temp.txt #get and calculate the md5sum of the page
+  wget -q $i -O $tempFile #get and calculate the md5sum of the page
   md5=`md5sum $tempFile | awk '{ print $1 }'`
+
   previousSum=`cat $finalFile | grep $i | awk '{print $2}'`
-  echo $?
-  if [ "$md5" == "$previousSum" ]
+
+  if [ "$previousSum" == "" ] #didn't find the that page from the previous run
   then
-    echo yes
+    echo $i INIT
   fi
+
+  if [ "$md5" != "$previousSum" ]#the pages have changed from the previous run of the script
+  then
+    echo $i
+  fi
+
   echo $i $md5 >> $filename
 done
+
+#remove the unnecessary files and rename the final file
 rm -f $finalFile
 rm -f $tempFile
 mv $filename $finalFile
